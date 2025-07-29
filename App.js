5cfae0c629493c8f200efb4f76e9f481d0929a73
Jahez Abraham Johny <jaj@sureflow.com>
2025-07-29 10:36:44 +0400

    update

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 
+
+// View is compiled to UIView in iOS and AndroidView in Android - This makes react-native cross-platform
 export default function App() {
+  console.log("App executed"); // use only for debugging purposes not in production
+  
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <Text>Hello World</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -16,5 +20,5 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }, 
 });
